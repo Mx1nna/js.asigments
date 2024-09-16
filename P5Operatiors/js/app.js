c06383d99e3f706edce4d9d95be7aa69544f49d5
Mx1nna <amiunv362@ihmeducation.se>
2024-09-16 09:40:01 +0200

    operators

---
+++ P5Operatiors/js/app.js
@@ -0,0 +1,34 @@
+let speedInput = 72;
+let speedOutput = 0 ;
+speedOutput = speedInput / 3.6;
+console.log(speedOutput);
+
+let speedInput = 3;
+let speedOutput = 180;
+speedOutput = speedInput * 60;
+console.log(speedOutput);
+
+let speedInput = 11;
+let speedOut = 2.75;
+speedOut = speedInput / 4;
+console.log(speedOut);
+
+let speedInput =11%4;
+let speedOutput = 3
+speedOutput = speedInput
+console.log(speedOutput);
+
+let speedInput = 2 ;
+let speedOutput = 12.56;
+speedOutput = speedInput * 3.14;
+console.log(speedOutput);
+
+let speedInput = 11;
+let speedOutput = -11;
+speedOutput = speedInput *-1
+console.log(speedOutput);
+
+let speedInput = 70%1.82;
+let speedOutput = 21.13
+speedInput = speedInput /
+
